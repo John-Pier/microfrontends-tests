76a27c4b71fc7908e6131a30bcdb64b80a519d5f
Nikita Popov <n.r.popov@tinkoff.ru>
2024-03-29 19:02:46 +0400

    init

--- test-frameworks/single-spa/apps/navbar/src/assets/init-spa.js
+++ test-frameworks/single-spa/apps/navbar/src/assets/init-spa.js
@@ -1,20 +1,20 @@
-Promise.all([
-  System.import('single-spa'),
-  System.import('single-spa-layout'),
-]).then(([singleSpa, singleSpaLayout]) => {
-  const { constructApplications, constructLayoutEngine, constructRoutes } =
-    singleSpaLayout;
-  const { registerApplication, start } = singleSpa;
-
-  const routes = constructRoutes(document.querySelector('#single-spa-layout'));
-  const applications = constructApplications({
-    routes,
-    loadApp({ name }) {
-      return System.import(name);
-    },
-  });
-  const layoutEngine = constructLayoutEngine({ routes, applications });
-  applications.forEach(registerApplication);
-
-  start();
-});
+!function () {
+  Promise.all([
+    System.import('single-spa'),
+    System.import('single-spa-layout'),
+  ]).then(([singleSpa, singleSpaLayout]) => {
+    const { constructApplications, constructLayoutEngine, constructRoutes } =
+      singleSpaLayout;
+    const { registerApplication, start } = singleSpa;
+    const routes = constructRoutes(document.querySelector('#single-spa-layout'));
+    const applications = constructApplications({
+      routes,
+      loadApp({ name }) {
+        return System.import(name);
+      },
+    });
+    const layoutEngine = constructLayoutEngine({ routes, applications });
+    applications.forEach(registerApplication);
+    start();
+  }).catch(err => console.warn(err));
+}()
