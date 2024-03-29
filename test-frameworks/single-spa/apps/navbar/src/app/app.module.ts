dd548927cc5f2e008efd20ee5b62fc3ea330619f
Nikita Popov <n.r.popov@tinkoff.ru>
2024-03-29 17:51:40 +0400

    init

--- test-frameworks/single-spa/apps/navbar/src/app/app.module.ts
+++ test-frameworks/single-spa/apps/navbar/src/app/app.module.ts
@@ -4,19 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
-import {HostHeaderComponent} from "./host-header";
+import { HostHeaderComponent } from '@base-monorepo/common';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EmptyRouteComponent,
-    HostHeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EmptyRouteComponent],
+  imports: [BrowserModule, AppRoutingModule, HostHeaderComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- test-frameworks/single-spa/apps/navbar/src/app/host-header/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './model'
-export * from './host-header.component'
-export * from './data.service'
