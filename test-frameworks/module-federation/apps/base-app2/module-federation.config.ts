1a296f673b634ae37baa65c0cce57babed327ea8
Nikita Popov <n.r.popov@tinkoff.ru>
2024-04-07 20:40:57 +0400

    add MF

--- test-frameworks/module-federation/apps/base-app2/module-federation.config.ts
+++ test-frameworks/module-federation/apps/base-app2/module-federation.config.ts
@@ -1,20 +1,20 @@
 import { ModuleFederationConfig } from '@nx/webpack';
 
-const coreLibraries = new Set<string>(['@angular/core']);
+// const coreLibraries = new Set<string>(['@angular/core']);
 
 const config: ModuleFederationConfig = {
   name: 'base-app2',
   exposes: {
     './Routes': 'apps/base-app2/src/app/remote-entry/entry.routes.ts',
   },
-  shared: (libraryName, defaultConfig) => {
-    if (coreLibraries.has(libraryName)) {
-      return defaultConfig;
-    }
-
-    // Returning false means the library is not shared.
-    return false;
-  },
+  // shared: (libraryName, defaultConfig) => {
+  //   if (coreLibraries.has(libraryName)) {
+  //     return defaultConfig;
+  //   }
+  //
+  //   // Returning false means the library is not shared.
+  //   return false;
+  // },
 };
 
 export default config;
--- test-frameworks/module-federation/apps/base-app2/webpack.config.ts
+++ test-frameworks/module-federation/apps/base-app2/webpack.config.ts
@@ -2,3 +2,13 @@ import { withModuleFederation } from '@nx/angular/module-federation';
 import config from './module-federation.config';
 
 export default withModuleFederation(config);
+
+// const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+//
+// export default  withModuleFederationPlugin({
+//   remotes: {
+//     'mfe1': "http://localhost:4201/remoteEntry.js"
+//   },
+//   shared: shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+//   sharedMappings: ['@demo/auth-lib'],
+// });
