130217ba7ee5c4f8fbdf8ff354377b5580ae5082
Nikita Popov <n.r.popov@tinkoff.ru>
2024-04-07 20:43:13 +0400

    add MF

--- test-frameworks/module-federation/apps/host/src/app/app.routes.ts
+++ test-frameworks/module-federation/apps/host/src/app/app.routes.ts
@@ -3,11 +3,11 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: 'base-app2',
+    path: 'app2',
     loadChildren: () => import('base-app2/Routes').then((m) => m.remoteRoutes),
   },
   {
-    path: 'base-app1',
+    path: 'app1',
     loadChildren: () => import('base-app1/Routes').then((m) => m.remoteRoutes),
   },
 ];
