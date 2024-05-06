b54137848782a65b2755eb81b8240dc24f1019d1
Nikita Popov <n.r.popov@tinkoff.ru>
2024-05-06 12:51:22 +0400

    init mf

---
+++ dynamic-mf-repo/apps/mf-app2/webpack.config.js
@@ -0,0 +1,20 @@
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+  name: 'mf-app2',
+
+  exposes: {
+    './Component': './apps/mf-app2/src/app/app.component.ts',
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    }),
+  },
+});
